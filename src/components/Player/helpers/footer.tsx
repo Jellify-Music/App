b40933ad852e966ac1aecd4bc7d8ce692e2a54fc
Ritesh Shukla <ritesh.shukla2@M-LD4JMWLW26.local>
2025-05-28 02:12:57 +0530

    Add Shuffle

--- src/components/Player/helpers/footer.tsx
+++ src/components/Player/helpers/footer.tsx
@@ -9,6 +9,7 @@ import { StackParamList } from '../../../components/types'
 import { useQueueContext } from '../../../providers/Player/queue'
 import { shuffleJellifyTracks } from '../shuffle'
 import TrackPlayer from 'react-native-track-player'
+import Toast from 'react-native-toast-message'
 
 export default function Footer({
 	navigation,
@@ -17,11 +18,28 @@ export default function Footer({
 }): React.JSX.Element {
 	const { playQueue, useSkip, setPlayQueue } = useQueueContext()
 
-	const handleShuffle = () => {
-		const shuffledQueue = shuffleJellifyTracks(playQueue)
-		setPlayQueue(shuffledQueue)
-		TrackPlayer.setQueue(shuffledQueue)
-		useSkip.mutate(0)
+	const handleShuffle = async () => {
+		const currentTrackIndex = (await TrackPlayer.getActiveTrackIndex()) ?? 0 // returns track index
+		const currentTrackData = playQueue[currentTrackIndex]
+		const currentPosition = await TrackPlayer.getProgress()
+		// Remove current track and shuffle the rest
+		const rest = [
+			...playQueue.slice(0, currentTrackIndex),
+			...playQueue.slice(currentTrackIndex + 1),
+		]
+		const shuffled = shuffleJellifyTracks(rest)
+		// Insert the current track back at the same index
+		shuffled.splice(currentTrackIndex, 0, currentTrackData)
+		// Update queue
+		setPlayQueue(shuffled)
+		Toast.show({
+			text1: 'Shuffled',
+			type: 'success',
+		})
+		//Dont remove this
+		// await TrackPlayer.setQueue(shuffled);
+		// await TrackPlayer.skip(currentTrackIndex);
+		// await TrackPlayer.seekTo(currentPosition.position	); // resume from same position
 	}
 	return (
 		<YStack justifyContent='flex-end'>
