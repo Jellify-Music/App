f8e23673ecc58226cada3ef99fc5e452ab1a8da9
Ritesh Shukla <riteshshukla2381@gmail.com>
2025-05-29 18:39:09 +0530

    Add Shuffle (#385)
    * Add Shuffle
    
    * Add Shuffle
    
    * Add Shuffle
    
    * Fix
    
    ---------
    
    Co-authored-by: Ritesh Shukla <ritesh.shukla2@M-LD4JMWLW26.local>

---
+++ src/components/Player/shuffle.ts
@@ -0,0 +1,15 @@
+import { JellifyTrack } from '@/src/types/JellifyTrack'
+
+export function shuffleJellifyTracks(tracks: JellifyTrack[]): {
+	shuffled: JellifyTrack[]
+	original: JellifyTrack[]
+} {
+	const shuffled = [...tracks] // Make a copy to avoid mutating the original array
+
+	for (let i = shuffled.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+	}
+
+	return { shuffled, original: tracks }
+}
