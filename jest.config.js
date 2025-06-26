cea8e21339bee65eb57e977aba5f183dddf15fd1
Violet Caulfield <violet@cosmonautical.cloud>
2025-06-26 09:22:04 -0500

    fix jest tests, add blur mocks

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
 	setupFilesAfterEnv: [
 		'./jest/setup.ts',
+		'./jest/setup-blur.ts',
 		'./jest/setup-carplay.ts',
 		'./jest/setup-device-info.js', // JS to prevent Typescript implicit any warning
 		'./jest/setup-reanimated.ts',
