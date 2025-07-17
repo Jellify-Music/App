19fad9ecc1e8daeaefe40bb771f5ace48945c59a
Violet Caulfield <violet@cosmonautical.cloud>
2025-07-16 21:07:32 -0500

    plex

--- src/components/Player/components/blurred-background.tsx
+++ src/components/Player/components/blurred-background.tsx
@@ -19,7 +19,16 @@ export default function BlurredBackground({
 	const blurhash = getPrimaryBlurhashFromDto(nowPlaying!.item)
 
 	return (
-		<ZStack flex={1} width={width} height={height} position='absolute'>
+		<ZStack
+			flex={1}
+			width={width}
+			height={height}
+			position='absolute'
+			top={0}
+			left={0}
+			bottom={0}
+			right={0}
+		>
 			<Blurhash
 				style={{
 					width,
