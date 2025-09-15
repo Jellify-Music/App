5ce4ff9ab14804250dd01bb73827dd808b6bf5ac
Violet Caulfield <violet@cosmonautical.cloud>
2025-09-15 06:41:58 -0500

    fixes for border radius on images not working on android

--- src/components/Global/components/image.tsx
+++ src/components/Global/components/image.tsx
@@ -1,14 +1,15 @@
 import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client/models'
 import { isUndefined } from 'lodash'
-import { getTokenValue, Token } from 'tamagui'
+import { getTokenValue, Token, View } from 'tamagui'
 import { useJellifyContext } from '../../../providers'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, ViewStyle } from 'react-native'
 import { ImageType } from '@jellyfin/sdk/lib/generated-client/models'
 import { NitroImage, useImage } from 'react-native-nitro-image'
 import { Blurhash } from 'react-native-blurhash'
 import { getBlurhashFromDto } from '../../../utils/blurhash'
 import Animated, { FadeIn, FadeOut } from 'react-native-reanimated'
 import { getItemImageUrl } from '../../../api/queries/image/utils'
+import { useMemo } from 'react'
 
 interface ItemImageProps {
 	item: BaseItemDto
@@ -59,53 +60,24 @@ interface ItemBlurhashProps {
 
 const AnimatedBlurhash = Animated.createAnimatedComponent(Blurhash)
 
-function ItemBlurhash({
-	item,
-	cornered,
-	circular,
-	width,
-	height,
-	testID,
-}: ItemBlurhashProps): React.JSX.Element {
+const Styles = StyleSheet.create({
+	blurhash: {
+		width: '100%',
+		height: '100%',
+	},
+})
+
+function ItemBlurhash({ item }: ItemBlurhashProps): React.JSX.Element {
 	const blurhash = getBlurhashFromDto(item)
 
-	const blurhashStyle = StyleSheet.create({
-		blurhash: {
-			borderRadius: cornered
-				? 0
-				: width
-					? getBorderRadius(circular, width)
-					: circular
-						? '100%'
-						: '5%',
-			width: !isUndefined(width)
-				? typeof width === 'number'
-					? width
-					: typeof width === 'string' && width.includes('%')
-						? width
-						: getTokenValue(width as Token)
-				: '100%',
-			height: !isUndefined(height)
-				? typeof height === 'number'
-					? height
-					: typeof height === 'string' && height.includes('%')
-						? height
-						: getTokenValue(height as Token)
-				: '100%',
-			alignSelf: 'center',
-			overflow: 'hidden',
-		},
-	})
-
-	return blurhash ? (
+	return (
 		<AnimatedBlurhash
+			resizeMode={'cover'}
+			style={Styles.blurhash}
 			blurhash={blurhash}
-			style={blurhashStyle.blurhash}
 			entering={FadeIn}
 			exiting={FadeOut}
 		/>
-	) : (
-		<></>
 	)
 }
 
@@ -130,54 +102,56 @@ function Image({
 	cornered,
 	testID,
 }: ImageProps): React.JSX.Element {
-	const { image, error } = useImage({ url: imageUrl })
+	const { image } = useImage({ url: imageUrl })
 
-	const imageStyle = StyleSheet.create({
-		image: {
-			borderRadius: cornered
-				? 0
-				: width
-					? getBorderRadius(circular, width)
-					: circular
-						? '100%'
-						: '5%',
-			width: !isUndefined(width)
-				? typeof width === 'number'
-					? width
-					: typeof width === 'string' && width.includes('%')
-						? width
-						: getTokenValue(width as Token)
-				: '100%',
-			height: !isUndefined(height)
-				? typeof height === 'number'
-					? height
-					: typeof height === 'string' && height.includes('%')
-						? height
-						: getTokenValue(height as Token)
-				: '100%',
-			alignSelf: 'center',
-			overflow: 'hidden',
-		},
-	})
+	const imageViewStyle = useMemo(
+		() =>
+			StyleSheet.create({
+				view: {
+					borderRadius: cornered
+						? 0
+						: width
+							? getBorderRadius(circular, width)
+							: circular
+								? getTokenValue('$20') * 10
+								: getTokenValue('$2'),
+					width: !isUndefined(width)
+						? typeof width === 'number'
+							? width
+							: typeof width === 'string' && width.includes('%')
+								? width
+								: getTokenValue(width as Token)
+						: '100%',
+					height: !isUndefined(height)
+						? typeof height === 'number'
+							? height
+							: typeof height === 'string' && height.includes('%')
+								? height
+								: getTokenValue(height as Token)
+						: '100%',
+					alignSelf: 'center',
+					overflow: 'hidden',
+				},
+			}),
+		[cornered, circular, width, height],
+	)
 
-	return image ? (
-		<AnimatedNitroImage
-			resizeMode='cover'
-			recyclingKey={imageUrl}
-			image={image}
-			testID={testID}
-			style={imageStyle.image}
-			entering={FadeIn}
-			exiting={FadeOut}
-		/>
-	) : (
-		<ItemBlurhash
-			item={item}
-			circular={circular}
-			width={width}
-			height={height}
-			cornered={cornered}
-		/>
+	return (
+		<View style={imageViewStyle.view} justifyContent='center' alignContent='center'>
+			{image ? (
+				<AnimatedNitroImage
+					resizeMode='cover'
+					recyclingKey={imageUrl}
+					image={image}
+					testID={testID}
+					entering={FadeIn}
+					exiting={FadeOut}
+					style={Styles.blurhash}
+				/>
+			) : (
+				<ItemBlurhash item={item} />
+			)}
+		</View>
 	)
 }
 
@@ -207,7 +181,7 @@ function getBorderRadius(
 				: typeof width === 'string' && width.includes('%')
 					? 0
 					: getTokenValue(width as Token) / 15
-	} else borderRadius = '5%'
+	} else borderRadius = getTokenValue('$2')
 
 	return borderRadius
 }
