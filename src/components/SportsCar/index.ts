02d6692f8ed9960aefb9a61c3fb7628d10166079
riteshshukla04 <riteshshukla2381@gmail.com>
2025-10-26 04:33:29 +0530

    feat: androidAUto

--- src/components/SportsCar/index.ts
+++ src/components/SportsCar/index.ts
@@ -8,19 +8,5 @@ export {
 	type MediaLibraryConverterConfig,
 } from './MediaLibraryConverter'
 
-// Export the example functions
-export {
-	mockBaseItemDtos,
-	initializeWithSingleFolder,
-	initializeWithMultipleFolders,
-	initializeWithConverter,
-	updateLibraryWithNewItems,
-	playSampleMedia,
-	createJellifyMediaLibrary,
-	createJellifyMultiFolderLibrary,
-	convertSingleItem,
-	convertMultipleItems,
-} from './MediaLibraryConverterExample'
-
 // Export the original example (commented out due to TypeScript errors)
 // export { default as AndroidAutoExample } from './AndroidAutoExample';
