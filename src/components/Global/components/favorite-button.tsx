a085bcdab72d0c30d7d3d4db1295b10cc7820424
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-10-30 13:33:13 -0500

    tightening up favorititing

--- src/components/Global/components/favorite-button.tsx
+++ src/components/Global/components/favorite-button.tsx
@@ -1,10 +1,10 @@
 import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client/models'
-import React from 'react'
+import React, { useCallback } from 'react'
 import Icon from './icon'
-import { isUndefined } from 'lodash'
-import { useJellifyUserDataContext } from '../../../providers/UserData'
 import Animated, { FadeIn, FadeOut } from 'react-native-reanimated'
+import { useAddFavorite, useRemoveFavorite } from '../../../api/mutations/favorite'
 import { useIsFavorite } from '../../../api/queries/user-data'
+import { Spinner } from 'tamagui'
 
 interface FavoriteButtonProps {
 	item: BaseItemDto
@@ -12,30 +12,32 @@ interface FavoriteButtonProps {
 }
 
 export default function FavoriteButton({ item, onToggle }: FavoriteButtonProps): React.JSX.Element {
-	const { toggleFavorite } = useJellifyUserDataContext()
+	const { data: isFavorite, refetch } = useIsFavorite(item)
 
-	const { data: isFavorite } = useIsFavorite(item)
+	const callback = useCallback(() => {
+		refetch()
+		if (onToggle) onToggle()
+	}, [refetch, onToggle])
 
 	return isFavorite ? (
+		<AddFavoriteButton item={item} onToggle={callback} />
+	) : (
+		<RemoveFavoriteButton item={item} onToggle={callback} />
+	)
+}
+
+function AddFavoriteButton({ item, onToggle }: FavoriteButtonProps): React.JSX.Element {
+	const { mutate, isPending } = useRemoveFavorite()
+
+	return isPending ? (
+		<Spinner color={'$primary'} width={'$2.5'} height={'$2'} />
+	) : (
 		<Animated.View entering={FadeIn} exiting={FadeOut}>
 			<Icon
 				name={'heart'}
 				color={'$primary'}
 				onPress={() =>
-					toggleFavorite(isFavorite, {
-						item,
-						onToggle,
-					})
-				}
-			/>
-		</Animated.View>
-	) : (
-		<Animated.View entering={FadeIn} exiting={FadeOut}>
-			<Icon
-				name={'heart-outline'}
-				color={'$primary'}
-				onPress={() =>
-					toggleFavorite(!!isFavorite, {
+					mutate({
 						item,
 						onToggle,
 					})
@@ -45,10 +47,23 @@ export default function FavoriteButton({ item, onToggle }: FavoriteButtonProps):
 	)
 }
 
-export function isFavoriteItem(item: BaseItemDto): boolean {
-	return isUndefined(item.UserData)
-		? false
-		: isUndefined(item.UserData.IsFavorite)
-			? false
-			: item.UserData.IsFavorite
+function RemoveFavoriteButton({ item, onToggle }: FavoriteButtonProps): React.JSX.Element {
+	const { mutate, isPending } = useAddFavorite()
+
+	return isPending ? (
+		<Spinner color={'$primary'} width={'$2.5'} height={'$2'} />
+	) : (
+		<Animated.View entering={FadeIn} exiting={FadeOut}>
+			<Icon
+				name={'heart-outline'}
+				color={'$primary'}
+				onPress={() =>
+					mutate({
+						item,
+						onToggle,
+					})
+				}
+			/>
+		</Animated.View>
+	)
 }
--- src/components/Global/components/favorite-context-menu-row.tsx
+++ src/components/Global/components/favorite-context-menu-row.tsx
@@ -5,6 +5,7 @@ import { useJellifyUserDataContext } from '../../../providers/UserData'
 import { Text } from '../helpers/text'
 import Animated, { FadeIn, FadeOut } from 'react-native-reanimated'
 import { useIsFavorite } from '../../../api/queries/user-data'
+import { useAddFavorite, useRemoveFavorite } from '../../../api/mutations/favorite'
 
 export default function FavoriteContextMenuRow({ item }: { item: BaseItemDto }): React.JSX.Element {
 	const { toggleFavorite } = useJellifyUserDataContext()
@@ -12,48 +13,56 @@ export default function FavoriteContextMenuRow({ item }: { item: BaseItemDto }):
 	const { data: isFavorite, refetch } = useIsFavorite(item)
 
 	return isFavorite ? (
-		<ListItem
-			animation={'quick'}
-			backgroundColor={'transparent'}
-			justifyContent='flex-start'
-			onPress={() => {
-				toggleFavorite(isFavorite, {
-					item,
-					onToggle: () => refetch(),
-				})
-			}}
-			pressStyle={{ opacity: 0.5 }}
-		>
-			<Animated.View
-				entering={FadeIn}
-				exiting={FadeOut}
-				key={`${item.Id}-remove-favorite-row`}
-			>
-				<XStack alignItems='center' justifyContent='flex-start' gap={'$2.5'}>
-					<Icon name={'heart'} small color={'$primary'} />
-
-					<Text bold>Remove from favorites</Text>
-				</XStack>
-			</Animated.View>
-		</ListItem>
+		<RemoveFavoriteContextMenuRow item={item} />
 	) : (
+		<AddFavoriteContextMenuRow item={item} />
+	)
+}
+
+function AddFavoriteContextMenuRow({ item }: { item: BaseItemDto }): React.JSX.Element {
+	const { mutate, isPending } = useAddFavorite()
+
+	return (
 		<ListItem
 			animation={'quick'}
 			backgroundColor={'transparent'}
 			justifyContent='flex-start'
 			onPress={() => {
-				toggleFavorite(!!isFavorite, {
-					item,
-					onToggle: () => refetch(),
-				})
+				mutate({ item })
 			}}
 			pressStyle={{ opacity: 0.5 }}
+			disabled={isPending}
 		>
 			<Animated.View entering={FadeIn} exiting={FadeOut} key={`${item.Id}-favorite-row`}>
 				<XStack alignItems='center' justifyContent='flex-start' gap={'$2.5'}>
 					<Icon small name={'heart-outline'} color={'$primary'} />
 
 					<Text bold>Add to favorites</Text>
+				</XStack>
+			</Animated.View>
+		</ListItem>
+	)
+}
+
+function RemoveFavoriteContextMenuRow({ item }: { item: BaseItemDto }): React.JSX.Element {
+	const { mutate, isPending } = useRemoveFavorite()
+
+	return (
+		<ListItem
+			animation={'quick'}
+			backgroundColor={'transparent'}
+			justifyContent='flex-start'
+			onPress={() => {
+				mutate({ item })
+			}}
+			pressStyle={{ opacity: 0.5 }}
+			disabled={isPending}
+		>
+			<Animated.View entering={FadeIn} exiting={FadeOut} key={`${item.Id}-favorite-row`}>
+				<XStack alignItems='center' justifyContent='flex-start' gap={'$2.5'}>
+					<Icon small name={'heart'} color={'$primary'} />
+
+					<Text bold>Remove from favorites</Text>
 				</XStack>
 			</Animated.View>
 		</ListItem>
