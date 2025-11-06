366804937be7fa87b4716d93b16673346c86d6ce
skalthoff <32023561+skalthoff@users.noreply.github.com>
2025-11-06 14:45:12 -0800

    Add DevRelease configuration and disable OTA updates for DevRelease builds

--- src/types/react-native-config.d.ts
+++ src/types/react-native-config.d.ts
@@ -2,6 +2,7 @@ declare module 'react-native-config' {
 	export interface NativeConfig {
 		OTA_UPDATE_ENABLED?: string
 		IS_MAESTRO_BUILD?: string
+		DEV_RELEASE?: string
 	}
 
 	export const Config: NativeConfig
