675817370330b7096aa9728ed0d0e3ed705b6a1a
riteshshukla04 <riteshshukla2381@gmail.com>
2025-11-15 00:54:26 +0530

    fix: Fix incorrect return of  on Web

--- scripts/sendDiscordMessage.js
+++ scripts/sendDiscordMessage.js
@@ -1,16 +1,14 @@
-
-
-const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL
 
 async function sendDiscordMessage(message) {
-  const res = await fetch(WEBHOOK_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({content:message}),
-  });
+	const res = await fetch(WEBHOOK_URL, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ content: message }),
+	})
 
-  console.log("Sent:", message);
+	console.log('Sent:', message)
 }
 
-const msg = process.argv.slice(2).join(" ");
-sendDiscordMessage(msg);
+const msg = process.argv.slice(2).join(' ')
+sendDiscordMessage(msg)
