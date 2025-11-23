f62feb6e049e1bf3e42685400e99701cf345ba38
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-11-23 09:52:12 -0600

    playlist screen redesign

--- src/components/Playlist/index.tsx
+++ src/components/Playlist/index.tsx
@@ -1,23 +1,28 @@
-import { Separator, useTheme, XStack } from 'tamagui'
+import { ScrollView, XStack } from 'tamagui'
 import Track from '../Global/components/track'
 import Icon from '../Global/components/icon'
-import { RefreshControl } from 'react-native'
 import { PlaylistProps } from './interfaces'
-import PlayliistTracklistHeader from './components/header'
 import { usePlaylistContext } from '../../providers/Playlist'
-import { runOnJS, useAnimatedScrollHandler } from 'react-native-reanimated'
-import AnimatedDraggableFlatList from '../Global/components/animated-draggable-flat-list'
 import { useNavigation } from '@react-navigation/native'
 import { RootStackParamList } from '../../screens/types'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import useHapticFeedback from '../../hooks/use-haptic-feedback'
-import { closeAllSwipeableRows } from '../Global/components/swipeable-row-registry'
+import Sortable from 'react-native-sortables'
+import { useCallback, useLayoutEffect } from 'react'
+import { useReducedHapticsSetting } from '../../stores/settings/app'
+import { RenderItemInfo } from 'react-native-sortables/dist/typescript/types'
+import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client'
 
 export default function Playlist({
 	playlist,
 	navigation,
 	canEdit,
 }: PlaylistProps): React.JSX.Element {
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerRight: () => canEdit && <Icon small name={editing ? 'save' : 'pencil'} />,
+		})
+	})
+
 	const {
 		scroll,
 		playlistTracks,
@@ -29,93 +34,67 @@ export default function Playlist({
 		useRemoveFromPlaylist,
 	} = usePlaylistContext()
 
-	const trigger = useHapticFeedback()
-
-	const theme = useTheme()
+	const [reducedHaptics] = useReducedHapticsSetting()
 
 	const rootNavigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
-	const scrollOffsetHandler = useAnimatedScrollHandler({
-		onBeginDrag: () => {
-			'worklet'
-			runOnJS(closeAllSwipeableRows)()
-		},
-		onScroll: (event) => {
-			'worklet'
-			scroll.value = event.contentOffset.y
-		},
-	})
+	const renderItem = useCallback(
+		({ item: track, index }: RenderItemInfo<BaseItemDto>) => (
+			<XStack alignItems='center' key={`${index}-${track.Id}`}>
+				<Sortable.Handle>
+					<Icon name='drag' />
+				</Sortable.Handle>
 
-	return (
-		<AnimatedDraggableFlatList
-			refreshControl={
-				<RefreshControl
-					refreshing={isPending}
-					onRefresh={refetch}
-					tintColor={theme.primary.val}
-				/>
-			}
-			contentInsetAdjustmentBehavior='automatic'
-			data={playlistTracks ?? []}
-			dragHitSlop={{ left: -50 }} // https://github.com/computerjazz/react-native-draggable-flatlist/issues/336
-			keyExtractor={(item, index) => {
-				return `${index}-${item.Id}`
-			}}
-			ItemSeparatorComponent={() => <Separator />}
-			ListHeaderComponent={() =>
-				PlayliistTracklistHeader(playlist, editing, playlistTracks ?? [], canEdit)
-			}
-			stickyHeaderIndices={[0]}
-			numColumns={1}
-			onDragBegin={() => {
-				trigger('impactMedium')
-			}}
-			onDragEnd={({ data, from, to }) => {
-				useUpdatePlaylist.mutate(
-					{
-						playlist,
-						tracks: data,
-					},
-					{
-						onSuccess: () => {
-							setPlaylistTracks(data)
-						},
-					},
-				)
-			}}
-			refreshing={isPending}
-			renderItem={({ item: track, getIndex, drag }) => (
 				<Track
 					navigation={navigation}
 					track={track}
 					tracklist={playlistTracks ?? []}
-					index={getIndex() ?? 0}
+					index={index}
 					queue={playlist}
 					showArtwork
 					onLongPress={() => {
-						if (editing) {
-							drag()
-						} else {
+						if (!editing)
 							rootNavigation.navigate('Context', {
 								item: track,
 								navigation,
 							})
-						}
 					}}
 					showRemove={editing}
-					onRemove={() =>
-						useRemoveFromPlaylist.mutate({ playlist, track, index: getIndex()! })
-					}
-					prependElement={
-						editing && canEdit ? <Icon name='drag' onPress={drag} /> : undefined
-					}
+					onRemove={() => useRemoveFromPlaylist.mutate({ playlist, track, index })}
 					isNested={editing}
 				/>
-			)}
-			style={{
-				marginHorizontal: 2,
-			}}
-			onScroll={scrollOffsetHandler}
-		/>
+			</XStack>
+		),
+		[navigation, playlist, playlistTracks, editing, useRemoveFromPlaylist],
+	)
+
+	return (
+		<ScrollView flex={1}>
+			<Sortable.Grid
+				data={playlistTracks ?? []}
+				keyExtractor={(item) => {
+					return `${item.Id}`
+				}}
+				columns={1}
+				customHandle
+				overDrag='vertical'
+				sortEnabled={canEdit && editing}
+				onDragEnd={({ fromIndex, toIndex, data }) => {
+					useUpdatePlaylist.mutate(
+						{
+							playlist,
+							tracks: data,
+						},
+						{
+							onSuccess: () => {
+								setPlaylistTracks(data)
+							},
+						},
+					)
+				}}
+				renderItem={renderItem}
+				hapticsEnabled={!reducedHaptics}
+			/>
+		</ScrollView>
 	)
 }
