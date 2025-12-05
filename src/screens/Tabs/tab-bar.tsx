2a72b45ce822385065b0fd5f1a1f6648e41b9324
Violet Caulfield <violet@cosmonautical.cloud>
2025-12-05 13:36:15 -0600

    meh

--- src/screens/Tabs/tab-bar.tsx
+++ src/screens/Tabs/tab-bar.tsx
@@ -2,6 +2,7 @@ import { Miniplayer } from '../../components/Player/mini-player'
 import InternetConnectionWatcher from '../../components/Network/internetConnectionWatcher'
 import useIsMiniPlayerActive from '../../hooks/use-mini-player'
 import { useIsFocused } from '@react-navigation/native'
+import { YStack } from 'tamagui'
 
 export default function TabBar(): React.JSX.Element {
 	const isFocused = useIsFocused()
@@ -9,9 +10,9 @@ export default function TabBar(): React.JSX.Element {
 	const isMiniPlayerActive = useIsMiniPlayerActive()
 
 	return (
-		<>
+		<YStack position='absolute'>
 			{isMiniPlayerActive && isFocused && <Miniplayer />}
 			<InternetConnectionWatcher />
-		</>
+		</YStack>
 	)
 }
