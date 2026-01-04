0f5bf9fa97708a5d7e9c6e67601fc58eac83d0ae
Violet Caulfield <violet@cosmonautical.cloud>
2026-01-04 13:54:09 -0600

    streamline this a lil bit

--- src/configs/library.config.ts
+++ src/configs/library.config.ts
@@ -2,6 +2,6 @@
  * The maximum number of items that will be in
  * a library list's recycle pool
  */
-const MAX_ITEMS_IN_RECYCLE_POOL = 20
+const MAX_ITEMS_IN_RECYCLE_POOL = 40
 
 export default MAX_ITEMS_IN_RECYCLE_POOL
