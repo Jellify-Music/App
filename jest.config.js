1e7051e6d50fa3662b2a2a141d0c05be0a6c5778
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-02-19 05:30:34 -0600

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
 	],
 	extensionsToTreatAsEsm: ['.ts', '.tsx'],
 	transformIgnorePatterns: [
-		'node_modules/(?!(@)?(react-native|react-native-.*|react-navigation|jellyfin|burnt|expo|expo-.*|shopify)/)',
+		'node_modules/(?!(@tamagui/config|@)?(react-native|react-native-.*|react-navigation|jellyfin|burnt|expo|expo-.*|shopify)/)',
 	],
 	moduleNameMapper: {
 		'^.+\\.ttf$': '<rootDir>/jest/setup/file-mock.ts',
