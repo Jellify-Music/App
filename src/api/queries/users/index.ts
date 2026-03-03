b3bf0b74408c289332466aef2b9ad52559e6d906
arijohn723 <ariahnajohnson@gmail.com>
2026-03-03 17:23:33 -0600

    hook to retrieve lists of users

---
+++ src/api/queries/users/index.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import { UserQueryKey } from './keys'
+import { getApi } from '@/src/stores'
+import { getUserApi } from '@jellyfin/sdk/lib/utils/api'
+
+//hook to get users on server
+export const useUsers = () => {
+	//using a query to call fetchUsers for server (not playlist)
+	return useQuery({ queryKey: UserQueryKey, queryFn: fetchUsers })
+}
+
+//function to call get user API (jellyfin), no export because it's only used here
+const fetchUsers = () => {
+	//use api (only get api when this function is called to get users)
+	const api = getApi()
+
+	//check set
+	if (!api) {
+		throw new Error('API Instance not set')
+	}
+
+	//return users from api
+	return getUserApi(api).getUsers()
+}
