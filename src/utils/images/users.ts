5096bd1c74ace27ea8c7cf6632bbff356a430b9a
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-04-04 11:47:05 -0500

    add additional scaffold

--- src/utils/images/users.ts
+++ src/utils/images/users.ts
@@ -1,5 +1,13 @@
+import { getApi } from '@/src/stores'
 import { UserDto } from '@jellyfin/sdk/lib/generated-client'
+import { getImageApi } from '@jellyfin/sdk/lib/utils/api'
 
 export default function getUserImageUrl(user: UserDto): string | undefined {
+	const api = getApi()
+
+	if (!api) return undefined
+
+	const imageApi = getImageApi(api)
+
 	return undefined
 }
