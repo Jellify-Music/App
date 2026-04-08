e443cb4eb1163079769a09df97777bb902ef8882
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-04-08 07:16:02 -0500

    animation fixes for android (plz fix thalia's bug)
    rollback eslint 10 upgrade, we need to wait for the react plugin to update

--- src/components/Global/helpers/animated-row.tsx
+++ src/components/Global/helpers/animated-row.tsx
@@ -18,9 +18,6 @@ export default function AnimatedRow({ children, testID }: AnimatedRowProps) {
 			entering={FadeIn.easing(Easing.in(Easing.ease)).reduceMotion(ReduceMotion.System)}
 			exiting={FadeOut.easing(Easing.out(Easing.ease)).reduceMotion(ReduceMotion.System)}
 			layout={LinearTransition.springify().reduceMotion(ReduceMotion.System)}
-			style={{
-				flex: 1,
-			}}
 		>
 			{children}
 		</Animated.View>
