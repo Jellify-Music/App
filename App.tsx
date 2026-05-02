99cd341ea478068e3e291d0b3e9b3ac1ce508a89
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-05-02 09:12:12 -0500

    convert tab navigator to static config

--- App.tsx
+++ App.tsx
@@ -4,18 +4,14 @@ import 'react-native-url-polyfill/auto'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import Jellify from './src/components/jellify'
 import { TamaguiProvider } from 'tamagui'
-import { LogBox, useColorScheme } from 'react-native'
+import { LogBox } from 'react-native'
 import jellifyConfig from './src/configs/tamagui.config'
 import { queryClient } from './src/constants/query-client'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { NavigationContainer } from '@react-navigation/native'
-import { getJellifyNavTheme } from './src/components/theme'
 import ErrorBoundary from './src/components/ErrorBoundary'
 import OTAUpdateScreen from './src/components/OtaUpdates'
 import { usePerformanceMonitor } from './src/hooks/use-performance-monitor'
-import navigationRef from './src/screens/navigation'
-import { useColorPresetSetting, useThemeSetting } from './src/stores/settings/app'
 import QueryPersistenceConfig from './src/configs/query-persistence.config'
 import registerTrackPlayer from './src/services/player'
 import configureDownloadManager from './src/services/downloads'
@@ -54,12 +50,6 @@ export default function App(): React.JSX.Element {
 }
 
 function Container(): React.JSX.Element {
-	const [theme] = useThemeSetting()
-	const [colorPreset] = useColorPresetSetting()
-
-	const isDarkMode = useColorScheme() === 'dark'
-	const resolvedMode = theme === 'system' ? (isDarkMode ? 'dark' : 'light') : theme
-
 	return (
 		<GestureHandlerRootView>
 			<ReducedMotionConfig mode={ReduceMotion.System} />
