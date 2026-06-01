d5da6c9396a19c0f7663d37d177d5192e1e378a8
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-06-01 07:20:43 -0500

    player artwork corner radius

--- src/components/Global/components/image.tsx
+++ src/components/Global/components/image.tsx
@@ -91,7 +91,7 @@ function getBorderRadius(
 			typeof width === 'number'
 				? width / 10
 				: typeof width === 'string' && width.includes('%')
-					? 0
+					? '5%'
 					: getTokenValue(width as Token) / 4
 	} else borderRadius = getTokenValue('$10')
 
