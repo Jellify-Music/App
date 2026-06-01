a5e00237fb0be2d1f330709f7fc81e09c4a7ab20
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-06-01 10:44:21 -0500

    MAKE TRACK IMAGES BIGGER

--- src/components/Global/components/Track/content.tsx
+++ src/components/Global/components/Track/content.tsx
@@ -118,8 +118,8 @@ export default function TrackRowContent({
 						<HideableArtwork>
 							<ItemImage
 								item={track}
-								width={'$3'}
-								height={'$3'}
+								width={'$4'}
+								height={'$4'}
 								imageOptions={{ maxWidth: 70, maxHeight: 70, quality: 90 }}
 							/>
 						</HideableArtwork>
