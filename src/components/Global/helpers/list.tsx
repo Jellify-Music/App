2f5e39d175ecec03712e859bf1b3fc6336fc2302
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-06-07 17:14:22 -0500

    make our List component way sexier with types and shit

--- src/components/Global/helpers/list.tsx
+++ src/components/Global/helpers/list.tsx
@@ -3,15 +3,12 @@ import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client'
 import { LegendList, LegendListProps, LegendListRef } from '@legendapp/list/react-native'
 import { Ref } from 'react'
 
-type ListProps<T extends (BaseItemDto | string | number) | BaseItemDto> = Omit<
-	LegendListProps<T>,
-	'keyExtractor' | 'recycleItems'
-> & {
+type ListType = (BaseItemDto | string | number) | BaseItemDto
+
+type ListProps<T extends ListType> = Omit<LegendListProps<T>, 'keyExtractor' | 'recycleItems'> & {
 	ref?: Ref<LegendListRef>
 }
 
-export default function List<T extends (BaseItemDto | string | number) | BaseItemDto>(
-	props: ListProps<T>,
-) {
+export default function List<T extends ListType>(props: ListProps<T>) {
 	return <LegendList<T> {...props} keyExtractor={ItemKeyExtractor} recycleItems />
 }
