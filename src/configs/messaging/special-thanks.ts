b44b4d31d2444d440f35696db212df1de9abcf57
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-07-05 22:09:18 -0500

    add special thanks

---
+++ src/configs/messaging/special-thanks.ts
@@ -0,0 +1,13 @@
+export const SPECIAL_THANKS = [
+	'Bill Thornton',
+	'BotBlake',
+	'Chaphasilor',
+	'Erik Balchunas',
+	'Lyssa VanDomelen',
+	'James Harvey',
+	'Niels van Velzen',
+	'Ritesh Shukla',
+	'Thalia Fields',
+	'skaltoff',
+	'skribe',
+]
