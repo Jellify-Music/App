944c699702035af3d655e8c6b3c3870e3fe120aa
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-07-05 22:38:13 -0500

    fix queue dismissal on iOS

--- src/providers/Player/index.tsx
+++ src/providers/Player/index.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, use } from 'react'
-import { StyleSheet } from 'react-native'
+import { Platform, StyleSheet } from 'react-native'
 import { usePagerView } from 'react-native-pager-view'
 
 interface PlayerContext {
@@ -15,7 +15,21 @@ interface PlayerProviderProps {
 }
 
 export const PlayerProvider = ({ children }: PlayerProviderProps) => {
-	const { AnimatedPagerView, ref, setPage } = usePagerView()
+	const { AnimatedPagerView, ref, setPage: setPagerViewPage } = usePagerView()
+
+	/**
+	 * Sets the page of the AnimatedPagerView.
+	 *
+	 * For iOS, a shim is required and implemented here
+	 *
+	 * On Android, business as usual
+	 *
+	 * @see https://github.com/callstack/react-native-pager-view#known-issues
+	 */
+	const setPage =
+		Platform.OS === 'ios'
+			? (index: number) => requestAnimationFrame(() => ref.current?.setPage(index))
+			: setPagerViewPage
 
 	return (
 		<PlayerContext
