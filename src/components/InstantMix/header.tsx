11fad2917a53e5e48cc9375b031a865eaf797216
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-07-16 17:40:55 -0500

    smol change to instant mix

--- src/components/InstantMix/header.tsx
+++ src/components/InstantMix/header.tsx
@@ -1,8 +1,7 @@
-import { H5, Square, useTheme, View, XStack, YStack, ZStack } from 'tamagui'
+import { H5, useTheme, XStack, YStack } from 'tamagui'
 import { useInstantMixContext } from '../../providers/InstantMix'
 import ItemImage from '../Global/components/image'
 import { getItemName } from '../../utils/formatting/item-names'
-import MaterialDesignIcons from '@react-native-vector-icons/material-design-icons'
 
 export default function MixTrackListHeader() {
 	const { item } = useInstantMixContext()
@@ -10,20 +9,19 @@ export default function MixTrackListHeader() {
 	const theme = useTheme()
 
 	return (
-		<YStack flex={1} marginTop={'$4'} gap={'$2'}>
+		<YStack flex={1} marginTop={'$4'} gap={'$2'} alignContent='center'>
 			<XStack justifyContent='center'>
-				<ZStack alignContent='center'>
-					<ItemImage
-						item={item}
-						width={'$15'}
-						height={'$15'}
-						imageOptions={{
-							maxHeight: 750,
-							maxWidth: 750,
-						}}
-						elevate
-					/>
-				</ZStack>
+				<ItemImage
+					item={item}
+					width={'$15'}
+					height={'$15'}
+					imageOptions={{
+						maxHeight: 750,
+						maxWidth: 750,
+					}}
+					elevate
+					circular
+				/>
 			</XStack>
 
 			<H5 lineBreakStrategyIOS='standard' textAlign='center'>
