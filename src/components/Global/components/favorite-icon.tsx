dcf2b4aedee7a4e4d9c60a7e94182e2e6a5a9a13
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-07-25 09:37:26 -0500

    prevent flicker on favorite icon

--- src/components/Global/components/favorite-icon.tsx
+++ src/components/Global/components/favorite-icon.tsx
@@ -9,21 +9,23 @@ import { useIsFavorite } from '../../../api/queries/user-data'
  * @param item - The item to display the favorite icon for.
  * @returns A React component that displays a favorite icon for a given item.
  */
-export default function FavoriteIcon({ item }: { item: BaseItemDto }): React.JSX.Element | null {
+export default function FavoriteIcon({ item }: { item: BaseItemDto }) {
 	const { data: isFavorite } = useIsFavorite(item)
 
-	return isFavorite ? (
-		<Icon
-			xsmall
-			name='heart'
-			color={'$primary'}
-			transition={'lazy'}
-			enterStyle={{
-				opacity: 0,
-			}}
-			exitStyle={{
-				opacity: 0,
-			}}
-		/>
-	) : null
+	return (
+		isFavorite && (
+			<Icon
+				xsmall
+				name='heart'
+				color={'$primary'}
+				transition={'lazy'}
+				enterStyle={{
+					opacity: 0,
+				}}
+				exitStyle={{
+					opacity: 0,
+				}}
+			/>
+		)
+	)
 }
